d938850c2f7536c521147c2e566250d4f1a14825
he9982 <105538039+he9982@users.noreply.github.com>
2024-12-02 10:17:25 -0800

    svm 1.0
    svm and some advanced tech

---
+++ SVM/read.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import numpy as np
+from PIL import Image
+import torch
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+
+
+# In[2]:
+
+
+class CustomImageFolder(Dataset):
+    def __init__(self, train_1_dir, train_0_dir, transform=None):
+        self.train_1_dir = train_1_dir
+        self.train_0_dir = train_0_dir
+        self.transform = transform
+        self.samples = []
+
+        # Add all images from train_te
+        for root, _, files in os.walk(train_1_dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                self.samples.append((file_path, 1))
+
+        # Add selected images from train_image
+        for root, _, files in os.walk(train_0_dir):
+            files = np.random.choice(files, 100, replace=False)
+            for file in files:
+                file_path = os.path.join(root, file)
+                self.samples.append((file_path, 0))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, index):
+        img_path, label = self.samples[index]
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+
+# In[3]:
+
+
+train_1_dir = "../train_1"
+test_1_dir = "../test_1"
+train_0_dir = "../train_0"
+test_0_dir = "../test_0"
+
+# Load the images and data loader for training and testing
+Dataset_tr = CustomImageFolder(train_1_dir, train_0_dir, transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+]))
+Dataset_te = CustomImageFolder(test_1_dir, test_0_dir, transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+]))
+
+#trainloader = torch.utils.data.DataLoader(dataset=Dataset_tr, batch_size=batch_size, shuffle=True)
+#testloader = torch.utils.data.DataLoader(dataset=Dataset_te, batch_size=batch_size, shuffle=True)
+
